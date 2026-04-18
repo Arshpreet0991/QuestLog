f222044cd804d0bcb4f16e85a9514f5e82ff7fef
Arshpreet0991 <arsh.singh0991@gmail.com>
2026-04-18 03:42:47 -0400

    merged conflict in api response

--- src/types/ApiResponse.ts
+++ src/types/ApiResponse.ts
@@ -3,9 +3,5 @@ export interface ApiResponse<T> {
   message: string;
   data?: T;
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> 8e7c8f7c5c760652b3b39d0effa8921ce8dc5948
 // data is optional, therefore the "?"
 // T is the just a placeholder, widely expected by community
