94ba49c1b0e09072490be8a376a999cde90a7277
Arshpreet0991 <arsh.singh0991@gmail.com>
2026-04-18 03:35:32 -0400

    merged conflicts in zod schemas

--- src/schemas/daySchema.ts
+++ src/schemas/daySchema.ts
@@ -1,5 +1,4 @@
 import { z } from "zod";
-<<<<<<< HEAD
 import { taskSchema } from "./taskSchema";
 
 export const daySchema = z.object({
@@ -11,13 +10,4 @@ export const daySchema = z.object({
     })
     .optional(),
   taskList: z.array(taskSchema),
-=======
-
-export const daySchema = z.object({
-  reflection: z.object({
-    wentRight: z.string().optional(),
-    wentWrong: z.string().optional(),
-    improve: z.string().optional(),
-  }),
->>>>>>> 8e7c8f7c5c760652b3b39d0effa8921ce8dc5948
 });
--- src/schemas/taskSchema.ts
+++ src/schemas/taskSchema.ts
@@ -1,15 +1,8 @@
 import { z } from "zod";
 
 export const taskSchema = z.object({
-<<<<<<< HEAD
-  task: z.string(),
-  taskType: z.enum(["mainQuest", "sideQuest"]),
-  category: z.enum(["body", "mind", "wealth", "relationships"]),
-  points: z.number(),
-=======
   content: z.string(),
   taskType: z.enum(["mainQuest", "sideQuest"]),
   category: z.enum(["body", "mind", "wealth"]),
   dayId: z.string(),
->>>>>>> 8e7c8f7c5c760652b3b39d0effa8921ce8dc5948
 });
